efb68f23e05ca1b263735b8c514d379bd9f67aa0
User <user@Users-MacBook-Air.local>
2023-10-28 13:46:07 +0800

    post hiile

--- user_api/main.js
+++ user_api/main.js
@@ -1,40 +1,51 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+
 const fs = require("fs");
-const { stringify } = require("querystring");
 
 const port = 8008;
 
 const app = express();
+
 app.use(express.json());
 
-app.get("/api/users", (req, res) => {
-  res.status(200).json({ message: "Succes GET method" });
-});
+//<Get start>
+
+// app.get("/api/users", (req, res) => {
+//   console.log("Get all users");
+//   res.status(200).json({ message: "Succes GET method" });
+// });
+
+//<Get end/>
+//<Post start>
 
 app.post("/api/users", (req, res) => {
-  console.log("REQ", req.body);
-
-  console.log("Create New User");
+  console.log("Create new user");
   const newUser = { id: uuidv4(), ...req.body };
 
   const { users } = JSON.parse(
-    fs.readFileSync("users.json", { enconding: "utf-8" })
-  );
+    fs.readFileSync("users.json", { encoding: "utf-8" })
+  ); // {users: []}
 
   users.push(newUser);
 
-  fs.writeFileSync("users.json", JSON.stringify({ users }), {
-    enconding: "utf-8",
+  fs.writeFileSync("./users.json", JSON.stringify({ users }), {
+    encoding: "utf-8",
   });
-  res.status(201).json({ message: "Succes POST method" });
+  res.status(201).json({ message: "success" });
 });
-//============
-app.put("/api/users/:id", (req, res) => {
-  res.status(200).json({ message: "Succes PUT method" }); //{users; []}
-});
+
+//<Post end/>
+//<Delete start>
+
 app.delete("/api/users/:id", (req, res) => {
   res.status(200).json({ message: "Succes DELETE method" });
+});
+
+//<Delete end/>
+
+app.put("/api/users/:id", (req, res) => {
+  res.status(200).json({ message: "Succes PUT method" }); //{users; []}
 });
 
 app.listen(port, () => console.log(`Server running at http:localhost:${port}`));
