500a8fbad1dd40fad68abf7227dd438575107bed
User <user@Users-MacBook-Air.local>
2023-11-03 15:19:46 +0800

    commit bug zaslaa

---
+++ lesson_node_backend/Data/usersData.jsx
@@ -0,0 +1,58 @@
+export const users = [
+  {
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    date: "20/02/2002",
+    job: "  Desktop Support Technician",
+    avatarUrl:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    date: "20/02/2002",
+    job: " Quality Control Specialist",
+    avatarUrl:
+      " https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&q=80&w=1587&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    date: "20/02/2003",
+    job: "	Tax Accountant",
+    avatarUrl:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    date: "20/02/2004",
+    job: "Office Assistant I",
+    avatarUrl:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    date: "20/02/2005",
+    job: "Community Outreach Specialist",
+    avatarUrl:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Corkery",
+    email: "Karley_Dach@jasper.info",
+    date: "20/02/2006",
+    job: "UI/UX Designer",
+    avatarUrl:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+];
---
+++ lesson_node_backend/components/userBodyInfo.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const userBodyInfo = ({ users }) => {
+  console.log("USERS", users);
+
+  return (
+    <>
+      {users.map((user) => {
+        return (
+          <tbody>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox mr-5 ml-10" />
+                </label>
+              </th>
+              <th>
+                <br />
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="w-12 h-8 mb-5 rounded-full">
+                      <img src={user.avatarUrl} alt="Avatar" />
+                    </div>
+                  </div>
+                </div>
+              </th>
+              <td>
+                <br />
+                <div>
+                  <div className="font-bold ml-5">{user.username}</div>
+                </div>
+              </td>
+              <td>
+                <br />
+                <div className="font-bold ml-5">{user.name}</div>
+              </td>
+              <td>
+                <span className="badge badge-ghost badge-sm ml-5">
+                  {user.job}
+                </span>
+              </td>
+
+              <td>
+                {" "}
+                <div className="text-sm opacity-50 ml-5">{user.email}</div>
+              </td>
+              <td>
+                <div className="text-sm opacity-50 ml-5">{user.date}</div>
+              </td>
+              <th>
+                <th>
+                  <button className="btn btn-outline btn-warning">Edit</button>
+                  git
+                </th>
+                <th>
+                  <button className="btn btn-outline btn-error ml-5 mr-10">
+                    Delete
+                  </button>
+                </th>
+              </th>
+            </tr>
+          </tbody>
+        );
+      })}
+    </>
+  );
+};
+
+export default userBodyInfo;
